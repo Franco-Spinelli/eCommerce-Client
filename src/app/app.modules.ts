473871e0f7973ba7f99f8a1c5789c5fd75a72808
Franco-Spinelli <spinellifranco04@gmail.com>
2024-06-22 16:25:15 -0300

    first commit

---
+++ src/app/app.modules.ts
@@ -0,0 +1,23 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
